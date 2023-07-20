6cc173f5713d96fb9f8b14489a89d353c225a971
NasibaU <naasibaa@yahoo.com>
2023-07-19 23:56:53 -0400

    Updating @B29G4-US156

---
+++ src/test/java/com/xFleet/stepDefinitions/FilterVehicles_StepDefinition.java
@@ -0,0 +1,39 @@
+package com.xFleet.stepDefinitions;
+
+import com.xFleet.pages.FilterVehiclesPage;
+import com.xFleet.utilities.BrowserUtils;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class FilterVehicles_StepDefinition {
+
+    FilterVehiclesPage filterVehiclesPage=new FilterVehiclesPage();
+
+
+    @Then("User clicks on filter icon, {string} button should be visible")
+    public void user_clicks_on_filter_icon_button_should_be_visible(String filterIcon) {
+        BrowserUtils.sleep(5);
+        filterVehiclesPage.filterIcon.click();
+    }
+
+    @When("User can apply filters by clicking on filter name, from {string} menu.")
+    public void user_can_apply_filters_by_clicking_on_filter_name_from_menu(String manageFiltersButton) {
+        BrowserUtils.sleep(5);
+        filterVehiclesPage.manageFiltersButton.click();
+
+        filterVehiclesPage.checkBoxOption1.click();
+
+    }
+
+    @Then("User can apply filters by typing the filter name, from {string} menu.")
+    public void user_can_apply_filters_by_typing_the_filter_name_from_menu(String filterInputBox) {
+        filterVehiclesPage.filterInputBox.sendKeys("Tags");
+
+        BrowserUtils.sleep(5);
+        filterVehiclesPage.tagsFilterCheckBox.click();
+        BrowserUtils.sleep(5);
+
+
+    }
+
+}
--- src/test/resources/features/FilterVehicles.feature
+++ src/test/resources/features/FilterVehicles.feature
@@ -2,10 +2,13 @@
 Feature: Filter vehicle table under Fleet-Vehicle page
 
 	Background:
-		Given user is on the logged into the webpage
+		Given user is on the login page
 	
 	@B29G4-155
 	Scenario: As a user, I should be able to filter vehicle table under Fleet-Vehicle page
-		Given User clicks on filter icon, "Manage Filter" button should be visible
+		Given user logged in as "driver"
+		When  user hover over on "fleetTab"
+		And user click on "vehicleModules" module
+		Then User clicks on filter icon, "Manage Filter" button should be visible
 		When User can apply filters by clicking on filter name, from 'Manage Filters' menu.
 		Then User can apply filters by typing the filter name, from 'Manage Filters' menu.
